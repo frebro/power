c9818939452840413e05ea7fa2e928fba754dd4f
AlexxHansson <alexxhansson@gmail.com>
2013-02-23 14:23:47 +0100

    Fetching data from GeoServer

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var http = require('http');
+var fs = require('fs');
+var data = "";
 
 function onRequest(request, response) {
   response.end();
@@ -13,27 +15,24 @@ console.log("Server has started.");
 var options = {
     host: '194.116.110.159',
     port: 8280,
-    path: '/geoserver/Belysning/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=Belysning:Karlshamn_belysning_armaturer_SWEREF_point&maxFeatures=50&outputFormat=json',
-    method: 'GET',
-    dataType: 'json'
-    //headers: { "api_key" : "MY API KEY HERE" }
+    path: '/geoserver/Belysning/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=Belysning:Karlshamn_belysning_armaturer_SWEREF_point&maxFeatures=10&outputFormat=json',
+    method: 'GET'
 };
 
 var req = http.request(options, function(res) {
-
-
+    var data = "";
     console.log('STATUS: ' + res.statusCode);
     res.setEncoding('utf8');
-    res.on('data', function (data) {
-    	
-    	//console.log(data);
-    	var poles = data.features;
-
-    	console.log(data.features);
-
-      //console.log('BODY: ' + test);
-        
+    res.on('data', function (chunk) {
+        data += chunk;
     });
+
+    res.on('end', function() {
+        var result = JSON.parse(data);
+        console.log( result.features[0].geometry );
+    });
+    
+    //console.log(data);
 });
 
 req.end();

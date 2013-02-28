e978398d310ed791bc4225418300a92c535decac
AlexxHansson <alexxhansson@gmail.com>
2013-02-28 11:44:44 +0100

    jQuery support added to server

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var http = require('http');
 var fs = require('fs');
 var app = express();
+var parseString = require('xml2js').parseString;
+var resultset = {};
 var lights = [];
 function onRequest(request, response) {
   response.end();
@@ -13,9 +15,12 @@ app.all('/*', function(req, res, next) {
   next();
 });
 
+app.get('/alllights', function(req, res){
+    res.send(200, resultset);
+});
+
 app.get('/lights', function(req, res){
-    var result = {};
-    res.send(200, lights);
+    res.send(200, resultset);
 });
 
 app.listen(8888);
@@ -23,7 +28,32 @@ console.log("Server has started on port 8888");
 
 //http://194.116.110.159:8280/geoserver/Belysning/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=Belysning:Karlshamn_belysning_armaturer_SWEREF_point&maxFeatures=50&outputFormat=json
 
+var options2 = {
+    host: 'api.yr.no',
+    port: 80,
+    path: '/weatherapi/sunrise/1.0/?lat=56.1706;lon=14.86188;date=2013-02-23',
+    method: 'GET'
+};
 
+var sunreq = http.request(options2, function(res){
+    var data = "";
+    res.setEncoding('utf8');
+    res.on('data', function(chunk){
+        data = chunk;
+    });
+
+    res.on('end', function(){
+        var xml = data;
+        parseString(xml, function(err, result){
+            var date = result.astrodata.time[0].$;
+            //var sunrise = result.astrodata.time[0].location[0].sun[0].$;
+            resultset.suncycle = result.astrodata.time[0].location[0].sun[0].$;
+            //console.log(sunrise);
+            //console.log(result.astrodata.time[0]);
+            //console.log(result.astrodata.time[0].location[0].sun[0].$);
+        }); 
+    })
+});
 
 var options = {
     host: '194.116.110.159',
@@ -33,7 +63,7 @@ var options = {
 };
 
 var req = http.request(options, function(res) {
-    var data = "";    
+    var data = "";
     console.log('STATUS: ' + res.statusCode);
     res.setEncoding('utf8');
     res.on('data', function (chunk) {
@@ -42,31 +72,40 @@ var req = http.request(options, function(res) {
 
     res.on('end', function() {
 
-        var light = {};
         var result = JSON.parse(data);
-        for(var i=0; i<result.features.length; i++){
-            light.coordinates = {
-               x : result.features[i].geometry.coordinates[1],
-               y : result.features[i].geometry.coordinates[0]
-            } 
-            light.plugged_in = result.features[i].properties.URKOPPLAD;
-            if(light.plugged_in == 'N') {
-                light.plugged_in = false;
-            } else {
-                light.plugged_in = true;
-            }
-            light.effect = parseFloat( result.features[i].properties.EFFEKT_W );
+        var totalwatts = 0;
+        var max = 0;
+        var min = 9999;
 
+        for(var i=0; i<result.features.length; i++){
+
+            var watt = parseInt( result.features[i].properties.EFFEKT_W );
+            var light = {
+                    coordinates : {
+                        x : result.features[i].geometry.coordinates[1],
+                        y : result.features[i].geometry.coordinates[0]
+                    },
+                    plugged_in : result.features[i].properties.URKOPPLAD,
+                    effect: watt
+            };
+            if(watt > max) max = watt;
+            if(watt < min) min = watt;
+            if(!isNaN(watt)) totalwatts += watt;
 
             lights.push(light);
             
         }
+        resultset.watts = totalwatts;
+        resultset.max = max;
+        resultset.min = min;
+        resultset.lights = lights;
+        
         console.log('Loading done!');
         console.log('Lights loaded: '+i);
+        console.log('Total watts loaded: '+totalwatts);
     });
-    
-    //console.log(data);
 });
 
 req.end();
+sunreq.end();
 console.log("Request sent!");
